6420281fed120297aacf484ab922482f7ffbf909
Melissarce <71056974+Melissarce@users.noreply.github.com>
2020-12-08 16:53:38 -0600

    Create loops.js

---
+++ JS/loops.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+			var person = new Array();
+		
+			person[0] = prompt("What is your Name?");
+			person[1] = prompt("Where do you live?");
+			person[2] = prompt("What is your occupation?");
+		
+			for (var i = 0; i < person.length; i++) {
+				document.getElementById(i).innerHTML = person[i];
+			}
+	document.getElementById("title").innerHTML = "Thank you for your information! You supplied this information:";
